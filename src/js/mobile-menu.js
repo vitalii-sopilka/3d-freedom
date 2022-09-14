9d597ad0b8532af72c5896478415cf9c150a2637
Lyubov16 <lyubov.vaskivna@gmail.com>
2022-09-14 16:18:05 +0300

    add include

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,23 +2,18 @@
   const refs = {
     openMenuBtn: document.querySelector('.js-open-menu'),
     closeMenuBtn: document.querySelector('.js-close-menu'),
-    menu: document.querySelector('.menu'),
+    menu: document.querySelector('.js-menu-container'),
     body: document.querySelector('body'),
-    menuList: document.querySelector('.menu__list'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-    refs.menuList.addEventListener('click', removeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
-    refs.body.classList.toggle('no-scroll');
-  }
-
-    function removeMenu() {
-    refs.menu.classList.add('is-hidden');
-   
+    refs.body.classList.toggle('scroll');
   }
 })();
+
+
+
